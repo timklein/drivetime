4c5896423ff2792c9b19019c9840bc071b4d72b3
Tim Klein <timklein@users.noreply.github.com>
2016-09-30 19:10:03 -0600

    Initial tie-in of the API

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # DriveTime
 
-*DriveTime* is a work in progress.
+*DriveTime* is a work in progress. The goal is to pull event data from the Eventful API located at http://api.eventful.com
 
 ## Usage
+
+You will need a free Eventful API key. Load this key into the appropriate place in the *config/configVars.example* file, then rename the file *configVars.json*
+
+Currently, loading the homepage will generate a console log in the terminal with the details of today's Denver events from the API and the event titles for page 1 of the results.
 
 ## To Do
 
 + ~~Base project start~~
-+ Format page layout
-+ Integrate Eventful API
++ ~~Format page layout~~
++ ~~Integrate Eventful API~~
++ Find a way to identify critical events
++ Integrate events into page
--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+var eventful = require('eventful-node');
+var config = require('./config/configVars.json');
+
+var client = new eventful.Client(config.eventfulAppKey);
+
+ 
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'pug');
@@ -8,7 +14,25 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
+	client.searchEvents({ location: 'denver', date: 'today' }, function(err, data){
+		if(err){
+			return console.error(err);
+		}
+  
+		console.log('Recieved ' + data.search.total_items + ' events in ' + data.search.page_count + ' pages');
+		console.log('Event listings: ');
+
+		console.log(data.search);
+  
+		//print the title of each event 
+		for(var i in data.search.events.event){
+			console.log(data.search.events.event[i].title);
+		}
+
+	});
+
 	res.render('index');
+
 });
 
 app.listen(3000, function() {
